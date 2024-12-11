6bf0a4981d7a9a56d57fbeadb03f23ad024f9864
sallyibrahim117 <sallyibrahim117@gmail.com>
2024-12-11 10:54:15 -0800

    f

--- src/app/Modules/auth/auth.module.ts
+++ src/app/Modules/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { SignupComponent } from './component/signup/signup.component';
+import { LoginComponent } from './component/login/login.component';
 
 
 @NgModule({
-  declarations: [AuthComponent, SignupComponent],
+  declarations: [AuthComponent, SignupComponent, LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
